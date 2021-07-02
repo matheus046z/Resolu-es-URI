e337ac5e37aad811f1e2c02f03b83e8bd624142c
Matheus <mt.1_@live.com>
2021-07-02 15:32:45 -0300

    Altera URI 1001 e cria URI 1002

---
+++ Beginners/Python/URI - 1002.py
@@ -0,0 +1,8 @@
+# Calcula a area da circunferencia de raio R
+# Input contem um valor de floating point
+# Resultado apresenta o valor "A" com quatro casas decimais
+
+π = 3.14159
+R = float(input())
+A = float(round(π * (R ** 2), 4))
+print("A=" + "{:.4f}".format(A))
