c4695b8f0a813ef6242999e26881d0787cadc7f9
Matheus <mt.1_@live.com>
2021-07-07 22:49:47 -0300

    Modifica URI-1005

--- Beginners/Python/URI-1005.py
+++ Beginners/Python/URI-1005.py
@@ -1,3 +1,8 @@
+# Problema:
+# Ler dois valores float A e B, correspondendo a duas notas
+# Calcular a media sendo que a nota A tem peso de 3.5 e a B 7.5
+
+
 A = float(input())
 B = float(input())
 p1 = 3.5
