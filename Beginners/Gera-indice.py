4168766fdfe96992a70339f00fc389d80dbd9e43
Matheus <mt.1_@live.com>
2021-07-07 21:50:05 -0300

    Cria Gera-indice

---
+++ Beginners/Gera-indice.py
@@ -0,0 +1,19 @@
+import glob
+
+files = glob.glob('/home/Matheus/Documentos/VS_Clone/URI_Problems-Solved/Beginners/Python/*.py')
+
+i = 0
+cortastr = []
+numera = []
+
+for i in range(len(files)):
+    cortastr.append(files[i][-28:])
+
+for i in range(len(cortastr)):
+    numera.append(cortastr[i][17:25])
+
+for i in range(len(files)):
+    print("["+numera[i]+"]("+cortastr[i]+")")
+
+
+
